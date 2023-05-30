fa093ee0859eb87a71581285d4a4b3be5c6888c5
Taki Kwan <nativeaaaa@gmail.com>
2023-05-30 11:00:00 +0800

    native1.0.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "",
+    description: "",
   },
   plugins: [
     "gatsby-transformer-sharp",
