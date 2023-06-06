b21105cbadbcf0e39502a1a8e8ec545aba1a6388
Taki Kwan <nativeaaaa@gmail.com>
2023-06-06 11:48:04 +0800

    native1.0.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
         stripQueryString: true,
       },
     },
+    "gatsby-plugin-cname",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
