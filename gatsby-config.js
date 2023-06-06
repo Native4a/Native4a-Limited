8cc41caef0b43470b832065263a08655479edcfd
Taki Kwan <nativeaaaa@gmail.com>
2023-06-06 12:30:56 +0800

    native1.0.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "NATIVE ADV LTD",
+    title: "",
     description: "",
     siteUrl: 'https://nativeaaaa.com.hk',
   },
