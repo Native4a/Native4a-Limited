18deb495e44225af84503e1f339389ee5ffecb47
Taki Kwan <nativeaaaa@gmail.com>
2023-06-07 11:18:09 +0800

    native1.0.3

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,17 +5,11 @@ import '../styles/global.css'
 import Seo from './seo'
 import Navigation from './header&footer/navigation'
 import Footer from './header&footer/footer'
-import Helmet from 'react-helmet'
 class Layout extends React.Component {
   render() {
     const { children } = this.props
     return (
       <>
-        {/* Custom metadata - assuming something coming from Node.js in pageContext prop */}
-        <Helmet>
-          <meta name="robots" content="noindex" />
-          <meta name="Googlebot" content="noindex" />
-        </Helmet>
         <Seo />
         <Navigation />
         <main>{children}</main>
