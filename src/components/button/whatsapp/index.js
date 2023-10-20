04c4ca099dc043e35192fd891137917dad2b10a7
Taki Kwan <nativeaaaa@gmail.com>
2023-10-20 17:13:55 +0800

    Native2.3.2

--- src/components/button/whatsapp/index.js
+++ src/components/button/whatsapp/index.js
@@ -7,7 +7,7 @@ const Whatsapp = () => {
 
     return (
         <div className='grid items-center'>
-            <AnchorLink to="https://api.whatsapp.com/send?phone=85267461301">
+            <AnchorLink to="https://api.whatsapp.com/send/?phone=85267461301&text=%E6%88%91%E6%83%B3%E6%9F%A5%E8%A9%A2Backlinks">
                 <button className="bg-emerald-500 text-white rounded-full px-7 py-1.5 font-medium text-base md:text-md xl:text-xl">
                     {whatsAppButton}
                 </button>
