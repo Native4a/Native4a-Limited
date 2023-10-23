fc0afd00d8d8396ddca4c6b19378f7b312f78e5d
Taki <beetles1994klw@gmail.com>
2023-10-24 01:36:45 +0800

    Native2.2.3

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,12 +49,12 @@ class RootIndex extends React.Component {
                   <p className="py-5 md:py-3 text-lg md:text-lg 2xl:text-2xl">{homePage.titleDescription.titleDescription}</p>
                   <div className="hidden md:grid grid-cols-1 gap-6 py-6">
                     <div className="flex gap-6 w-full items-center">
-                      <SocialMediaBtn />
+                      <SocialMediaBtn/>
                     </div>
                   </div>
                   <div className="md:hidden">
                     <div className="flex gap-2 w-full items-center">
-                      <SocialMediaBtn />
+                      <SocialMediaBtn/>
                     </div>
                   </div>
                 </div>
