e81f59610f5ced462df88419e26611886b6ec67e
Taki Kwan <nativeaaaa@gmail.com>
2023-12-21 18:27:23 +0800

    Native2.3.3

--- src/components/baseTools/Grid/index.js
+++ src/components/baseTools/Grid/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Grid = ({ children, gridContainer, gridCols }) => {
 
     //set default class style with tailwindCSS
-    const container = "container mx-auto pt-20";
+    const container = "container mx-auto pt-20 md:pt-20";
     const cols = "grid grid-cols-1 md:grid-cols-2 gap-6";
 
     return (
