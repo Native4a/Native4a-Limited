6fd0b627453f485e1ad2d7b775ce9c71f31a01fc
Taki Kwan <nativeaaaa@gmail.com>
2024-01-18 16:09:51 +0800

    Nat_2.3.4

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,84 +27,113 @@ import 'reactjs-popup/dist/index.css'
 
 //import ArticlePreview from "../components/article-preview"
 class RootIndex extends React.Component {
-
   render() {
     //const posts = get(this, "props.data.allContentfulBlogPost.nodes")
-    const [homePage] = get(this, "props.data.allContentfulHomePage.nodes")
-
+    const [addNewPage] = get(this, "props.data.allContentfulAddNewPage.nodes")
+    //For ServiceScope data
+    const inContactBox1 = addNewPage.contact[0].fields.textContact;
+    const inContactBox2 = addNewPage.contact[1].fields.title.en_US; // 從第一個元素中獲取 textContact
+    //console.log(inContactBox2);
+    //由於heading為陣列資料，而非直接包含 fields 屬性。必須定義資料才可使用
+    //如果你只想收集兩次迭代的結果，你可以在 map() 函式中添加一個計數器slice()，以限制迭代的次數。
+    //例子:const headingContents = addNewPage.heading.slice(0, 2).map((item) =>
+    const headingContents = addNewPage.heading.map((item) =>
+      item.fields.content.en_US
+    );
+    const contactBox = addNewPage.contact.map((item) =>
+      item.fields.title.en_US
+    );
     return (
       <Layout location={this.props.location}>
         <Seo
-          title={homePage.metaTitle}
-          description={homePage.metaDescription}
+          title={addNewPage.seo.fields.metaTitle.en_US}
+          description={addNewPage.seo.fields.metaDescription.en_US}
           ogUrl="https://nativeaaaa.com.hk/"
         />
         <Section SectionClass={styles.bgCustomGradient}>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 pt-24 lg:pt-22 xl:pt-22 2xl:pt-36 pb-0 lg:pb-14 xl:pb-16 2xl:pb-30">
-              <div className="px-6 xl:px-0 py-0 xl:py-6">
-                <div className="pt-2 md:pt-3 2xl:pt-0">
-                  <h1 className={styles.title}>{homePage.title}</h1>
-                  <h1 className={styles.subTitle}>{homePage.subTitle}</h1>
-                  <div className={styles.border}></div>
-                  <p className="py-5 md:py-3 text-lg md:text-lg 2xl:text-2xl">{homePage.titleDescription.titleDescription}</p>
-                  <div className="hidden md:grid grid-cols-1 gap-6 py-6">
-                    <div className="flex gap-6 w-full items-center">
-                      <SocialMediaBtn/>
-                    </div>
-                  </div>
-                  <div className="md:hidden">
-                    <div className="flex gap-2 w-full items-center">
-                      <SocialMediaBtn/>
-                    </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 pt-24 lg:pt-22 xl:pt-22 2xl:pt-36 pb-0 lg:pb-14 xl:pb-16 2xl:pb-30">
+            <div className="px-6 xl:px-0 py-0 xl:py-6">
+              <div className="pt-2 md:pt-3 2xl:pt-0">
+                {
+                  //<h1 className={styles.title}>{addNewPage.heading[0].fields.content.en_US} 不能用
+                  //Error: Cannot read properties of undefined (reading 'content')
+                  //請看定義區的headingContents
+                }
+                {headingContents.map((content, index) => {
+                  if (index === 0) {
+                    return <h1 className={styles.title}>{content}</h1>;
+                  } else if (index === 1) {
+                    return <div>
+                      <h2 className={styles.subTitle}>{content}</h2>
+                      <div className={styles.border}></div>
+                    </div>;
+                  } else if (index === 2) {
+                    return <p className="py-5 md:py-3 text-lg md:text-lg 2xl:text-2xl">{content}</p>;
+                  } else {
+                    return null;
+                  }
+                })}
+                <div className="hidden md:grid grid-cols-1 gap-6 py-6">
+                  <div className="flex gap-6 w-full items-center">
+                    <SocialMediaBtn />
                   </div>
                 </div>
-                <AwardsWeb/>
+                <div className="md:hidden">
+                  <div className="flex gap-2 w-full items-center">
+                    <SocialMediaBtn />
+                  </div>
+                </div>
               </div>
-              <div className="grid content-center z-0">
-                <Popup
-                  trigger={
-                    <div className={styles.overlay_container}>
-                      <img src={plsStep} alt="Girl in a jacket" width="500" height="600" loading="lazy" decoding="async" />
-                    </div>
-                  }
-                  modal
-                  contentStyle=""
-                >
-                  {(close) => (
-                    <div className={styles.modal}>
-                      <button className={styles.close} onClick={close}>X</button>
-                      {/**Put your youtube link here*/}
-                      <div className={styles.videoContainer}>
-                        <iframe
-                          className={styles.responsiveIframe}
-                          width="100%"
-                          src="https://www.youtube.com/embed/B53Pg3CyDTo"
-                          title="YouTube video player"
-                          frameborder="0"
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                          allowfullscreen
-                        ></iframe>
-                      </div>
-                    </div>
-                  )}
-                </Popup>
-              </div>
-              <Awards/>
+              <AwardsWeb />
             </div>
+            <div className="grid content-center z-0">
+              <Popup
+                trigger={
+                  <div className={styles.overlay_container}>
+                    <img src={plsStep} alt="Girl in a jacket" width="500" height="600" loading="lazy" decoding="async" />
+                  </div>
+                }
+                modal
+                contentStyle=""
+              >
+                {(close) => (
+                  <div className={styles.modal}>
+                    <button className={styles.close} onClick={close}>X</button>
+                    {/**Put your youtube link here*/}
+                    <div className={styles.videoContainer}>
+                      <iframe
+                        className={styles.responsiveIframe}
+                        width="100%"
+                        src="https://www.youtube.com/embed/B53Pg3CyDTo"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                  </div>
+                )}
+              </Popup>
+            </div>
+            <Awards />
+          </div>
         </Section>
         <section className={styles.sec2_wrap}>
-          <div className={styles.container}>
-            <div className="grid grid-rows-0">
-              <div className="relative container mx-auto">
-                <h2 className="flex items-center text-2xl pt-12 ml-2 font-bold">
-                  <span><FaSquare /></span>
-                  <b className="ml-3">{homePage.subTitle2}</b></h2>
-                <h2 className="text-2xl md:text-4xl py-3 ml-0 md:ml-4 leading-0 md:leading-[3.5rem]">{homePage.subTitle21}</h2>
-                <p className="text-xl mx-4">{homePage.subTitle21Description.subTitle21Description}</p>
-              </div>
-              <ServiceScope />
-            </div>
+          <div className="grid grid-rows-0 relative container mx-auto">
+            <h2 className="flex items-center text-2xl pt-12 ml-2 font-bold">
+              <span><FaSquare /></span>
+              {contactBox.map((title, index) => {
+                if (index === 0) {
+                  return <b className="ml-3">{title}</b>;
+                } else {
+                  return null;
+                }
+              })}
+            </h2>
+            <h2 className="text-2xl md:text-4xl py-3 ml-0 md:ml-4 leading-0 md:leading-[3.5rem]">{inContactBox1.en_US[0].fields.content.en_US}</h2>
+            <p className="text-xl mx-4">{inContactBox1.en_US[1].fields.content.en_US}</p>
           </div>
+          <ServiceScope />
         </section>
         <section className={styles.sec3_wrap}>
           <div className={styles.container}>
@@ -112,10 +141,10 @@ class RootIndex extends React.Component {
               <div className="inline md:hidden">
                 <h2 className="flex items-center text-2xl ml-4 my-16">
                   <span><FaSquare /></span>
-                  {homePage.subTitle22}</h2>
+                  {inContactBox2}</h2>
               </div>
               <div className="hidden md:inline text-center">
-                <h2 className="text-4xl underline decoration-yellow-400 my-14">{homePage.subTitle22}</h2>
+                <h2 className="text-4xl underline decoration-yellow-400 my-14">{inContactBox2}</h2>
               </div>
               <Clients />
             </div>
@@ -174,24 +203,45 @@ export const pageQuery = graphql`
           company
         }
       }
-      allContentfulHomePage {
+      allContentfulAddNewPage(filter: {contentful_id: {eq: "2vVfX03PDoKVGEDgRJgMz3"}}) {
         nodes {
-          title
-          subTitle
-          nativeLogo {
-            url
+          contentful_id
+          id
+          order
+          pageName
+          seo {
+            fields {
+              metaDescription {
+                en_US
+              }
+              metaTitle {
+                en_US
+              }
+            }
           }
-          titleDescription {
-            titleDescription
+          heading {
+            fields {
+              content {
+                en_US
+              }
+            }
           }
-          subTitle2
-          subTitle21
-          subTitle22
-          subTitle21Description {
-            subTitle21Description
+          contact {
+            fields {
+              title {
+                en_US
+              }
+              textContact {
+                en_US {
+                  fields {
+                    content {
+                      en_US
+                    }
+                  }
+                }
+              }
+            }
           }
-          metaTitle
-          metaDescription
         }
       }
     }
