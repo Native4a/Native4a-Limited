ef0e4df8189b3c94eba6d134833a884ca4d35b37
Taki Kwan <nativeaaaa@gmail.com>
2024-06-25 15:53:54 +0800

    BuildVersion:v2.3.240620-1432

--- src/components/Chatbase/index.js
+++ src/components/Chatbase/index.js
@@ -1,48 +1,30 @@
 import React, { useEffect } from 'react'
 
-const ChatbaseChatbot = () => {
+const Chatbot = () => {
   useEffect(() => {
-    // 添加 Chatbase 腳本到文檔中
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.async = true
-    script.innerHTML = `
-      (function(d, w, c) {
-        w.Chatbase = w.Chatbase || function() {
-          (w.Chatbase.q = w.Chatbase.q || []).push(arguments);
-        };
-        var s = d.createElement('script');
-        s.async = true;
-        s.src = 'https://app.chatbase.co/embed.js';  // 這裡是您的嵌入代碼 URL
-        var x = d.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-      })(document, window);
-      Chatbase('init', {
-        botId: 'sNjFjcxTAIrNBn1h0RyDj',  // 替換為您的機器人 ID
-        welcomeMessage: '您好！今天我能幫您什麼？',
-      });
+    const script1 = document.createElement('script')
+    script1.innerHTML = `
+      window.embeddedChatbotConfig = {
+        chatbotId: "sNjFjcxTAIrNBn1h0RyDj",
+        domain: "www.chatbase.co"
+      }
     `
-    document.body.appendChild(script)
+    document.body.appendChild(script1)
+
+    const script2 = document.createElement('script')
+    script2.src = 'https://www.chatbase.co/embed.min.js'
+    script2.setAttribute('chatbotId', 'sNjFjcxTAIrNBn1h0RyDj')
+    script2.setAttribute('domain', 'www.chatbase.co')
+    script2.defer = true
+    document.body.appendChild(script2)
 
     return () => {
-      // 在組件卸載時清理腳本
-      document.body.removeChild(script)
+      document.body.removeChild(script1)
+      document.body.removeChild(script2)
     }
   }, [])
 
-  return (
-    <div>
-      {/* 在這裡嵌入您的 iframe 代碼，並添加 title 屬性 */}
-      <iframe
-        src="https://app.chatbase.co/chatbot-embed/YOUR_BOT_ID"
-        width="100%"
-        height="500px"
-        frameBorder="0"
-        allow="microphone; geolocation"
-        title="Chatbase Chatbot" // 添加 title 屬性
-      ></iframe>
-    </div>
-  )
+  return null
 }
 
-export default ChatbaseChatbot
+export default Chatbot
