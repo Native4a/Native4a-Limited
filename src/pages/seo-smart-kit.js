0ae98a5ff44752768b6073ff1c7819b9006594c5
Taki Kwan <nativeaaaa@gmail.com>
2024-08-27 16:36:27 +0800

    BuildVersion: v3.1.240827-1635

--- src/pages/seo-smart-kit.js
+++ src/pages/seo-smart-kit.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Seo from '../components/seo'
 import Section from '../components/baseTools/Section'
 import Layout from '../components/layout'
+import { motion } from 'framer-motion'
 
 //CSS here//
 import * as styles from '../styles/seo-smart-kit.module.css'
@@ -25,6 +26,7 @@ class SEOsmartKit extends Component {
     savedCompCalc: 0,
     // 新增一個狀態來保存輸入的關鍵字
     savedKeyword: '',
+    savedData: [], // 用來儲存所有資料的陣列
   }
 
   VolumeCalc = (event) => {
@@ -100,13 +102,19 @@ class SEOsmartKit extends Component {
   }
 
   saveAllCalc = () => {
-    this.setState({
-      savedKeyword: this.state.Keyword,
-      savedVolCalc: this.state.volCalc,
-      savedQuarCalc: this.state.quarCalc,
-      savedYearCalc: this.state.yearCalc,
-      savedCompCalc: this.state.compCalc,
-    })
+    const newData = {
+      Keyword: this.state.Keyword,
+      volCalc: this.state.volCalc,
+      quarCalc: this.state.quarCalc,
+      yearCalc: this.state.yearCalc,
+      compCalc: this.state.compCalc,
+      vqycSum: this.state.vqycSum, // 將 vqycSum 添加到 newData 對象中
+    }
+
+    // 只保留最新的5筆資料
+    this.setState((prevState) => ({
+      savedData: [newData, ...prevState.savedData.slice(0, 4)],
+    }))
   }
 
   updateSum = () => {
@@ -115,18 +123,22 @@ class SEOsmartKit extends Component {
     this.setState({ vqycSum: sum })
   }
 
+  resetSavedData = () => {
+    this.setState({ savedData: [] })
+  }
+
   render() {
     return (
       <Layout>
         <Seo title="" description="" ogUrl="" />
-        <Section SectionClass="flex justify-evenly" ContainerClass="grid gap-6">
+        <Section SectionClass="flex justify-evenly" ContainerClass="grid">
           <div className="grid gap-4 justify-evenly pt-32 lg:pt-22 xl:pt-22 2xl:pt-36">
-            <span>
+            <span className="mb-5">
               <h1 className="text-5xl text-center">SEO Calculator</h1>
-              <p className="text-center">(測試版)</p>
+              <p className="text-center">(Web測試版)</p>
             </span>
           </div>
-          <div className="grid grid-cols-3 shadow-xl rounded-3xl bg-white p-6 mb-20 bg-white-500/[.06]">
+          <div className="grid grid-cols-3 shadow-xl rounded-3xl bg-white p-6 mb-8 bg-white-500/[.06]">
             <div className="grid gap-4 xl:py-14 xl:pl-16">
               <div className="flex flex-col gap-2">
                 <h3>Keyword：</h3>
@@ -200,9 +212,7 @@ class SEOsmartKit extends Component {
                   </div>
                   <div className="flex flex-col justify-between border-2 rounded-3xl p-5">
                     <h4>
-                      Three
-                      <br />
-                      Months
+                      3 Months
                       <br />
                       Change
                     </h4>
@@ -232,32 +242,90 @@ class SEOsmartKit extends Component {
                   >
                     Save
                   </button>
-                  <span className="flex justify-end text-xs pt-2">
-                    BuildVersion:v2.3.240826-1717 by taki
-                  </span>
                 </div>
               </div>
             </div>
+            <div className="grid col-span-3 justify-start text-xs pt-2">
+              <p>規則：</p>
+              <ol>
+                <li>目前只提供電腦版，平板 和 手機版待開發中。</li>
+                <li>
+                  儲存區最多可以儲存5筆資料。若再儲存超過5筆資料的話，新的一筆資料將會自動覆蓋最舊的資料。
+                </li>
+                <li>
+                  儲存的資料只是暫存，如果網站被更新，儲存的資料將被重置。
+                </li>
+                <li>若想自行保存資料，可以自行截圖保存下來。</li>
+              </ol>
+              <span>BuildVersion: v3.1.240827-1635 by Native4A</span>
+            </div>
           </div>
         </Section>
-        <Section ContainerClass="grid gap-6 justify-around">
-          <div class="grid grid-cols-5 gap-6 shadow-xl rounded-3xl bg-white p-6 mb-20 bg-white-500/[.06]">
-            <div class="text-lg font-bold">
-              Keyword: <span>{this.state.savedKeyword}</span>
-            </div>
-            <div class="flex text-lg font-bold justify-center">
-              <span>{this.state.savedVolCalc}</span>分
-            </div>
-            <div class="flex text-lg font-bold justify-center">
-              <span>{this.state.savedQuarCalc}</span>分
-            </div>
-            <div class="flex text-lg font-bold justify-center">
-              <span>{this.state.savedYearCalc}</span>分
-            </div>
-            <div class="flex text-lg font-bold justify-center">
-              <span>{this.state.savedCompCalc}</span>分
+        <Section ContainerClass="grid gap-6 justify-around mb-10">
+          <div Class="container mx-auto mt-5">
+            <div Class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+              <div Class="flex col-span-2 text-xl">
+                <h3>Keyword儲存區</h3>
+              </div>
+              <div Class="flex col-span-2 justify-end text-md">
+                <button
+                  onClick={this.resetSavedData}
+                  className="rounded-3xl px-10 text-white bg-red-600"
+                >
+                  ⟲ Reset
+                </button>
+              </div>
             </div>
           </div>
+          <div className="grid grid-cols-6 p-6">
+            <div className="text-lg font-bold">
+              Keyword: <span></span>
+            </div>
+            <div className="flex text-lg font-bold justify-center border-r-2 px-3">
+              <span>search volume</span>
+            </div>
+            <div className="flex text-lg font-bold justify-center border-r-2 px-3">
+              <span>3 Months Change</span>
+            </div>
+            <div className="flex text-lg font-bold justify-center border-r-2 px-3">
+              <span>Yearly changes</span>
+            </div>
+            <div className="flex text-lg font-bold justify-center px-3">
+              <span>Competition Score</span>
+            </div>
+            <div className="flex text-lg font-bold justify-end px-3">
+              <span>Total Score</span>
+            </div>
+          </div>
+          {this.state.savedData.map((data, index) => (
+            <motion.div
+              key={index}
+              className="grid grid-cols-6 gap-6 shadow-xl rounded-3xl bg-white p-6 bg-white-500/[.06]"
+              initial={{ opacity: 0, y: -20 }} // 初始狀態：透明且向上偏移
+              animate={{ opacity: 1, y: 0 }} // 動畫狀態：完全可見且回到原位
+              exit={{ opacity: 0, y: -20 }} // 離開狀態：透明且向上偏移
+              transition={{ duration: 0.5 }} // 動畫持續時間
+            >
+              <div className="text-md font-semibold">
+                <span>{data.Keyword}</span>
+              </div>
+              <div className="flex text-md font-semibold justify-center">
+                <span>{data.volCalc}</span>分
+              </div>
+              <div className="flex text-md font-semibold justify-center">
+                <span>{data.quarCalc}</span>分
+              </div>
+              <div className="flex text-md font-semibold justify-center">
+                <span>{data.yearCalc}</span>分
+              </div>
+              <div className="flex text-md font-semibold justify-center">
+                <span>{data.compCalc}</span>分
+              </div>
+              <div className="flex text-md font-semibold justify-end">
+                <span>{data.vqycSum}</span>分
+              </div>
+            </motion.div>
+          ))}
         </Section>
       </Layout>
     )
