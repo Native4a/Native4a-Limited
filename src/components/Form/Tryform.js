a0e985c4fb4a4381371bb2067b5d20942e65c15e
Taki Kwan <nativeaaaa@gmail.com>
2024-11-29 18:43:02 +0800

    BuildVersion: v3.1.241129-1842

--- src/components/Form/Tryform.js
+++ src/components/Form/Tryform.js
@@ -81,6 +81,7 @@ const Tryform = () => {
             className="appearance-none block w-full bg-gray-200 text-gray-500 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
             type="number"
+            max="8"
             placeholder="Phone Number"
             name="user_contact-number"
             aria-label="user_contact-number"
