e442972942e863d2e8a5703338bb14c603cddbc4
Taki Kwan <nativeaaaa@gmail.com>
2025-01-14 17:46:20 +0800

    BuildVersion: v3.1.250114-1746 [Add] 左好多野，數唔晒

--- src/components/Google site verification.js
+++ src/components/Google site verification.js
@@ -1,6 +1,9 @@
-import Helmet from "react-helmet";
-<Helmet>
+import Helmet from 'react-helmet'
+;<Helmet>
   <html lang="en" />
   <title>{site.siteMetadata.title}</title>
-<meta name="google-site-verification" content="0sBPGmcAVH9nFih2VD-l7R85I9X-kN2Wo_5eGyZMZTA" />
-</Helmet>;
+  <meta
+    name="google-site-verification"
+    content="0sBPGmcAVH9nFih2VD-l7R85I9X-kN2Wo_5eGyZMZTA"
+  />
+</Helmet>
