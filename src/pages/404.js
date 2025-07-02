9180efa46bdd6c37e288931de3539abe941f3907
Native4a <nativeaaaa@gmail.com>
2025-07-02 16:02:00 +0800

    Test

--- src/pages/404.js
+++ src/pages/404.js
@@ -21,7 +21,7 @@ const NotFoundPage = () => (
                 404: Not Found the page!!
               </Text>
               <Text tag="p">
-                You just hit a route that doesn&#39;t exist... the sadness.{' '}
+                You just hit a route that doesn&#39;t exist... the sadness.😉{' '}
               </Text>
             </div>
           </div>
